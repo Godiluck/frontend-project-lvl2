522d2b9346477c8a7d5438a7aa46005cbd6515bf
Godiluck <givan3819@gmail.com>
2021-12-12 00:21:03 +0300

    file1.json, file2.json & func.js added

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,9 +1,19 @@
 #!/usr/bin/env node
 
 import {program} from "commander";
+import {getFile, parseFiles} from "./func.js";
+
 
 program
     .description('Compares two configuration files and shows a difference.')
-    .option('-V, --version', 'output the version number');
+    .option('-f, --format [type]', 'output format')
+    .arguments('<filepath1> <filepath2>')
+    .option('-V, --version', 'output the version number')
+    .action((filepath1, filepath2) => {
+        const data1 = getFile(filepath1)
+        const data2 = getFile(filepath2)
+        const parsedJson = parseFiles(data1, data2)
+        console.log(parsedJson)
+    })
 
 program.parse();
